87d0248cee68bf86a5e88205da08af1f6a3255ff
adlet <skldf>
2018-10-31 15:35:15 +0600

    add migrate

---
+++ my_store/db/migrate/20181031093059_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+t.float :price
+t.string :name
+t.boolean :real
+t.float :weight
+      t.timestamps
+
+    
+    end
+  end
+end
