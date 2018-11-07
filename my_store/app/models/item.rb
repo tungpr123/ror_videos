4d7eb5891ef07203350e21d2ea2682d2b00fb309
adlet <skldf>
2018-11-07 14:49:40 +0600

    video6

--- my_store/app/models/item.rb
+++ my_store/app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+
+  validates :price,  numericality: { greater_than: 0, allow_nil: true }
+  validates :name, :description, presence: true
+
+  belongs_to :category
+
+  after_initialize {} # Item.new; Item.first
+  after_save {} # Item.save; Item.create item.update_attributes()
+  after_create { category.inc(:items_count, 1) }
+  after_update {}
+  after_destroy { category.inc(:items_count, -1) } # item.destroy
+
 end
--- my_store/db/migrate/20181031093059_create_items.rb
+++ my_store/db/migrate/20181031093059_create_items.rb
@@ -5,9 +5,10 @@ t.float :price
 t.string :name
 t.boolean :real
 t.float :weight
+t.string :description
       t.timestamps
 
-    
+
     end
   end
 end
